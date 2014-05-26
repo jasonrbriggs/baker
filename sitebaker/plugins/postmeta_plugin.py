0540f586075bbfbc6db6db610856a722f1b0af47
Jason R Briggs <jasonrbriggs@gmail.com>
2014-05-26 12:36:10 +0100

    add 'sub-title' handling, add 'permalink-url'

--- sitebaker/plugins/postmeta_plugin.py
+++ sitebaker/plugins/postmeta_plugin.py
@@ -5,8 +5,15 @@ def process(page, index=0):
     global tag_repeat_count
     if 'posted-on' in page.headers:
         page.template.set_value('posted-on', 'Posted on %s' % page.get_posted_date(), index)
+        page.template.set_value('post-date', page.get_posted_date(), index)
+    if 'title' in page.headers:
+        page.template.set_value('title', page.headers['title'], index)
+    if 'sub-title' in page.headers:
+        page.template.set_value('sub-title', page.headers['sub-title'], index)
     page.template.set_value('permalink', 'Permalink', index)
     page.template.set_attribute('permalink', 'href', page.url + '.html', index)
+    page.template.set_value('permalink-url', page.url, index)
+    page.template.set_attribute('permalink-url', 'href', page.url + '.html', index)
     return page
 
 add_filter('post-meta', process)
