7c37e59e3c50a552b095436a5de8f51760ed58c3
Jason R Briggs <mail@jasonrbriggs.com>
2019-07-25 20:06:46 +1200

    deleting python version

--- sitebaker/plugins/indexer_plugin.sql
+++
@@ -1,6 +0,0 @@
-create table search (
-url varchar(500),
-title varchar(500),
-search_text tsvector,
-actual_text text,
-last_modified timestamp);
--- test/__init__.py
+++
@@ -1,3 +0,0 @@
-import glob
-import os
-__all__ = [ os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*_test.py")]
